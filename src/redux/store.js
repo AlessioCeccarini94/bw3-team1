e57b027de1d12677f69b99007b8da01063307a61
AlessioCeccarini94 <alessioceccarini23@icloud.com>
2025-12-09 15:34:09 +0100

    experiences

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,11 @@ import { configureStore } from "@reduxjs/toolkit"
 import profileReducer from "./reducers/profileSlice"
 import experiencesReducer from "./reducers/experiencesReducer"
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     profile: profileReducer,
     experiences: experiencesReducer,
   },
 })
+
+export default store
