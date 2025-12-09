afbbeacf09b0c384bdd93436c6f34031793064a5
Enxos27 <vincenzo.calvaruso17@gmail.com>
2025-12-09 12:05:29 +0100

    Create ProfileInfo & Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import ProfileInfo from './components/ProfileInfo'
+import Footer from './components/Footer';
 
 function App() {
 
 
   return (
     <>
+    <ProfileInfo></ProfileInfo> 
+    <Footer/>
     </>
   )
 }
