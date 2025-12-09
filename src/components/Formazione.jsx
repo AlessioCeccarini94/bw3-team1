b85c306b9aa04e8904a8ff2993aefba445edde1c
Niccolò <niccolo.sorrenti@gmail.com>
2025-12-09 15:36:51 +0100

    commit

---
+++ src/components/Formazione.jsx
@@ -0,0 +1,17 @@
+import { BsPlusLg } from 'react-icons/bs';
+import { BsFillPencilFill } from 'react-icons/bs';
+
+const Formazione = function () {
+  return (
+    <>
+      <div className="justify-content-between align-items-center">
+        <h4>Formazione</h4>
+        <span>
+          <BsPlusLg /> <BsFillPencilFill />
+        </span>
+      </div>
+    </>
+  );
+};
+
+export default Formazione;
