812f7ed3184c3904c1c8444e356f63c67db284f4
Niccolò <niccolo.sorrenti@gmail.com>
2025-12-09 12:06:10 +0100

    Informazioni done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import './App.css'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
-
-
-  return (
-    <>
-    </>
-  )
+  return <></>;
 }
 
-export default App
+export default App;
