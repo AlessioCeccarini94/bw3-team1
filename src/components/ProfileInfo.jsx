0dd34a547e50978afbfd3e694fab3749dd071736
AlessioCeccarini94 <alessioceccarini23@icloud.com>
2025-12-10 14:44:31 +0100

    Adds_user_img_change

--- src/components/ProfileInfo.jsx
+++ src/components/ProfileInfo.jsx
@@ -23,10 +23,6 @@ const ProfileInfo = () => {
 
   const fileInputRef = useRef(null)
 
-  const handleButtonClick = () => {
-    fileInputRef.current.click()
-  }
-
   const handleFileChange = (e) => {
     const file = e.target.files[0]
     if (!file) return
@@ -108,108 +104,118 @@ const ProfileInfo = () => {
 
   // Render del componente
   return (
-    <Card className="shadow-sm border-0 mx-auto mt-3 mb-3">
-      {/* Immagine di Copertina (Fittizia) */}
-      <div
-        className="bg-dark"
-        style={{
-          height: "200px",
-          borderRadius: "8px 8px 0 0",
-          overflow: "hidden",
-        }}
-      >
-        <Card.Img
-          variant="top"
-          src={utente.coverUrl}
-          alt="Immagine di Copertina"
-          className="w-100 h-100"
-          style={{ objectFit: "cover" }}
-        />
-      </div>
-
-      {/* Immagine del Profilo */}
-      <Card.Body className="pt-0">
-        <div className="position-relative">
-          <Image
-            src={profileImg}
-            alt="Immagine del Profilo"
-            className="border border-4 border-white rounded-circle position-absolute"
-            style={{
-              width: "120px",
-              height: "120px",
-              objectFit: "cover",
-              marginTop: "-60px",
-              marginLeft: "1rem",
-              zIndex: 10,
-            }}
+    <>
+      <input
+        type="file"
+        accept="image/*"
+        ref={fileInputRef}
+        style={{ display: "none" }}
+        onChange={handleFileChange}
+      />
+      <Card className="shadow-sm border-0 mx-auto mt-3 mb-3">
+        {/* Immagine di Copertina (Fittizia) */}
+        <div
+          className="bg-dark"
+          style={{
+            height: "200px",
+            borderRadius: "8px 8px 0 0",
+            overflow: "hidden",
+          }}
+        >
+          <Card.Img
+            variant="top"
+            src={utente.coverUrl}
+            alt="Immagine di Copertina"
+            className="w-100 h-100"
+            style={{ objectFit: "cover" }}
           />
-          <Button
-            variant="primary"
-            className="rounded-circle d-flex align-items-center justify-content-center position-absolute"
-            style={{
-              left: "12%",
-              zIndex: 11,
-            }}
-            onClick={() => {
-              // Implementa la logica per la modifica dell'immagine
-              console.log("Modifica immagine del profilo")
-            }}
-          >
-            +
-          </Button>
         </div>
-        <div style={{ height: "60px" }}></div>
-        {/*  Informazioni Principali */}
-        <Container fluid className="px-3">
-          <Row className="align-items-start mb-2">
-            {/* Colonna di Sinistra: Nome e Dettagli */}
-            <Col>
-              <h4 className="mb-0 fw-bold">
-                {/* Nome e Cognome */}
-                {utente.nome} {utente.surname}{" "}
-                <span className="ms-2 text-muted fw-normal fs-6">
-                  {utente.pronoun}
-                </span>
-              </h4>
-            </Col>
-            {/* Colonna di Destra: Azienda e Università (Fittizie) */}
-            <Col xs="auto" className="text-end">
-              <div className="d-flex align-items-center text-secondary mb-1 justify-content-end">
-                <span className="fw-bold">{utente.scuolaEducator}</span>
-              </div>
-              <div className="d-flex align-items-center text-secondary justify-content-end">
-                <span className="fw-bold">{utente.universita}</span>
-              </div>
-            </Col>
-          </Row>
 
-          {/* Titolo e Posizione */}
-          <p className="mb-1">{utente.title}</p>
-          <div className="d-flex align-items-center text-muted fs-6 mb-1">
-            <span>
-              {utente.posizione} •{" "}
-              <a
-                href={`mailto:${utente.email}`}
-                className="text-decoration-none"
-              >
-                Informazioni di contatto
-              </a>
-            </span>
+        {/* Immagine del Profilo */}
+        <Card.Body className="pt-0">
+          <div className="position-relative">
+            <Image
+              src={profileImg}
+              alt="Immagine del Profilo"
+              className="border border-4 border-white rounded-circle position-absolute"
+              style={{
+                width: "120px",
+                height: "120px",
+                objectFit: "cover",
+                marginTop: "-60px",
+                marginLeft: "1rem",
+                zIndex: 10,
+              }}
+            />
+            <Button
+              variant="primary"
+              className="rounded-circle d-flex align-items-center justify-content-center position-absolute"
+              style={{
+                left: "12%",
+                zIndex: 11,
+              }}
+              onClick={() => {
+                fileInputRef.current.click()
+                // Implementa la logica per la modifica dell'immagine
+                console.log("Modifica immagine del profilo")
+              }}
+            >
+              +
+            </Button>
           </div>
-          <p className="mt-2 mb-3 fw-bold">Più di 500 collegamenti</p>
-        </Container>
-        {/*  Pulsanti di Azione */}
-        <div className="p-3">
-          <Button variant="primary" className="me-2">
-            Messaggio
-          </Button>
-          <Button variant="outline-secondary" className="me-2">
-            In sospeso
-          </Button>
-          <Button variant="outline-secondary">Altro</Button>
-        </div>
-      </Card.Body>
-    </Card>
+          <div style={{ height: "60px" }}></div>
+          {/*  Informazioni Principali */}
+          <Container fluid className="px-3">
+            <Row className="align-items-start mb-2">
+              {/* Colonna di Sinistra: Nome e Dettagli */}
+              <Col>
+                <h4 className="mb-0 fw-bold">
+                  {/* Nome e Cognome */}
+                  {utente.nome} {utente.surname}{" "}
+                  <span className="ms-2 text-muted fw-normal fs-6">
+                    {utente.pronoun}
+                  </span>
+                </h4>
+              </Col>
+              {/* Colonna di Destra: Azienda e Università (Fittizie) */}
+              <Col xs="auto" className="text-end">
+                <div className="d-flex align-items-center text-secondary mb-1 justify-content-end">
+                  <span className="fw-bold">{utente.scuolaEducator}</span>
+                </div>
+                <div className="d-flex align-items-center text-secondary justify-content-end">
+                  <span className="fw-bold">{utente.universita}</span>
+                </div>
+              </Col>
+            </Row>
+
+            {/* Titolo e Posizione */}
+            <p className="mb-1">{utente.title}</p>
+            <div className="d-flex align-items-center text-muted fs-6 mb-1">
+              <span>
+                {utente.posizione} •{" "}
+                <a
+                  href={`mailto:${utente.email}`}
+                  className="text-decoration-none"
+                >
+                  Informazioni di contatto
+                </a>
+              </span>
+            </div>
+            <p className="mt-2 mb-3 fw-bold">Più di 500 collegamenti</p>
+          </Container>
+          {/*  Pulsanti di Azione */}
+          <div className="p-3">
+            <Button variant="primary" className="me-2">
+              Messaggio
+            </Button>
+            <Button variant="outline-secondary" className="me-2">
+              In sospeso
+            </Button>
+            <Button variant="outline-secondary">Altro</Button>
+          </div>
+        </Card.Body>
+      </Card>
+    </>
   )
 }
 
