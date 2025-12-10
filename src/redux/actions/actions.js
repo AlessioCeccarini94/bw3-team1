87347d7ca51098a58be0556789bb3a744d675e05
AlessioCeccarini94 <alessioceccarini23@icloud.com>
2025-12-10 08:58:12 +0100

    n

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -2,7 +2,6 @@ export const FETCH_EXPERIENCES = "FETCH_EXPERIENCES"
 export const ADD_EXPERIENCES = "ADD_EXPERIENCES"
 
 export const fetchExperiencesAction = (userId) => {
-  // eslint-disable-next-line no-unused-vars
   const URL = `https://striveschool-api.herokuapp.com/api/profile/${userId}/experiences`
   const token =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OTM3ZGI0OGQzMjJmNTAwMTUxMDc2YTEiLCJpYXQiOjE3NjUyNzQ4ODMsImV4cCI6MTc2NjQ4NDQ4M30.Q9Y9RBdw6vYbWZ6d5on0z8oXE_EA5RSmRYfa__uTGkY"
